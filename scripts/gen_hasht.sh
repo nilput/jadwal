67b820f7477423932255878d5e6d0505aa2a3966
nilput <nilputs@gmail.com>
2020-07-16 06:18:52 +0000

    updated header generation script

--- scripts/gen_hasht.sh
+++ scripts/gen_hasht.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INPUT_HEADER="$SCRIPTDIR/../src/hasht.h"
+EXTRA_HEADER="$SCRIPTDIR/../src/div_32_funcs.h"
 gen() {
     prefix="$1";
     output="$2";
@@ -10,4 +11,5 @@ if [ $# -lt 2 ]; then
     echo "error, script usage:\
 ./gen_hasht [prefix] [output_header_name]" >&2;
 fi
+cp "$EXTRA_HEADER" "$(dirname "$2")"
 gen "$1" "$2";
